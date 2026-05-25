9ce2fa9d691bd6fd0fd9bc11efb9fe8a0544eb92
Dan Flatt <108902970+Flattorama@users.noreply.github.com>
2026-05-24 21:37:03 -0400

    Render interactive executive report directly

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import CISInfographicSite from "./components/CISInfographicSite.jsx";
-import ExecutiveReportAdditions from "./components/ExecutiveReportAdditions.jsx";
+import InteractiveReportSite from "./components/InteractiveReportSite.jsx";
 import "./styles.css";
 import "./executive-report-additions.css";
+import "./interactive-report.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CISInfographicSite />
-    <ExecutiveReportAdditions />
+    <InteractiveReportSite />
   </React.StrictMode>,
 );
