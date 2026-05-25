7a3f16fddb9c665f81e7917eb71fdc34848193cc
Dan Flatt <108902970+Flattorama@users.noreply.github.com>
2026-05-24 20:34:02 -0400

    Render executive report additions

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import CISInfographicSite from "./components/CISInfographicSite.jsx";
+import ExecutiveReportAdditions from "./components/ExecutiveReportAdditions.jsx";
 import "./styles.css";
+import "./executive-report-additions.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CISInfographicSite />
+    <ExecutiveReportAdditions />
   </React.StrictMode>,
 );
